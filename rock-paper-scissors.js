d9fbf72af98c13fc751df20fb543a4c468a11274
Jonah Breslow <jonah.breslow@mindbodyonline.com>
2023-08-27 14:01:19 -0700

    game loop w/spaghetti code

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -4,32 +4,81 @@ var Choice;
     Choice["Paper"] = "Paper";
     Choice["Scissors"] = "Scissors";
 })(Choice || (Choice = {}));
+var startButton = document.getElementById("start-game");
+var tryAgain = document.getElementById("try-again");
+var gameBoard = "<div id=\"game-container\">\n      <div\n        class=\"flex flex-wrap gap50 justify-center bottom-margin-20 top-margin-50\"\n      >\n        <div>\n          <h2>Human</h2>\n          <div class=\"flex justify-center gap5 border-solid padding-100\">\n            <button id=\"Rock\">Rock</button>\n            <button id=\"Paper\">Paper</button>\n            <button id=\"Scissors\">Scissors</button>\n          </div>\n          <h3 id=\"human-score\">Score:</h3>\n        </div>\n        <div>\n          <h2>Computer</h2>\n          <div class=\"flex justify-center gap5 border-solid padding-100\">\n            <button id=\"cRock\">Rock</button>\n            <button id=\"cPaper\">Paper</button>\n            <button id=\"cScissors\">Scissors</button>\n          </div>\n          <h3 id=\"computer-score\">Score:</h3>\n        </div>\n      </div>\n      <h3 id=\"roundAnnouncement\"></h3>\n    </div>";
+startButton.addEventListener("click", function () {
+    var _a;
+    var elem = document.getElementById("start-game-div");
+    console.log(elem);
+    (_a = elem.parentNode) === null || _a === void 0 ? void 0 : _a.removeChild(elem);
+    elem = document.getElementById("container");
+    elem.innerHTML = gameBoard;
+});
 var playerScore = 0;
 var computerScore = 0;
+var announceText;
 var nTies = 0;
-var buttons = document.querySelectorAll("button");
-buttons.forEach(function (button) {
-    button.addEventListener("click", function () {
+document
+    .getElementById("container")
+    .addEventListener("click", function (event) {
+    var button = event.target;
+    console.log(button);
+    if (button &&
+        button instanceof HTMLButtonElement &&
+        button.id != "start-game" &&
+        button.id != "try-again") {
         var playerChoice = Choice[button.id];
         var computerChoice = getComputerChoice();
         var score = playRound(playerChoice, computerChoice);
         if (score === 1) {
             playerScore += 1;
+            announceText = "You win! ".concat(playerChoice, " beats ").concat(computerChoice);
         }
         else if (score === -1) {
             computerScore += 1;
+            announceText = "You lose :( ".concat(computerChoice, " beats ").concat(playerChoice);
         }
         else {
             nTies += 1;
+            announceText = "It's a tie: You both chose ".concat(computerChoice);
         }
-        var humanResult = "Human: ".concat(playerScore);
-        var computerResult = "Computer : ".concat(computerScore);
+        var humanResult = "Score: ".concat(playerScore);
+        var computerResult = "Score: ".concat(computerScore);
         var humanScoreDisplay = document.getElementById("human-score");
         var computerScoreDisplay = document.getElementById("computer-score");
+        var roundAnnouncement = document.getElementById("roundAnnouncement");
+        if (playerScore === 5) {
+            endGame("Humans Win!");
+        }
+        else if (computerScore === 5) {
+            endGame("Machines Win :(");
+        }
+        else {
+            roundAnnouncement.textContent = announceText;
+        }
         humanScoreDisplay.textContent = humanResult;
         computerScoreDisplay.textContent = computerResult;
-    });
+    }
 });
+function resetGame() {
+    playerScore = 0;
+    computerScore = 0;
+    nTies = 0;
+}
+function endGame(text) {
+    var _a;
+    var elem = document.getElementById("game-container");
+    (_a = elem.parentNode) === null || _a === void 0 ? void 0 : _a.removeChild(elem);
+    elem = document.getElementById("container");
+    elem.innerHTML = "<h2>GAME OVER</h2>\n  <h3>".concat(text, "</h3>\n  <div\n        id=\"start-game-div\"\n        class=\"flex justify-center align-center full-height\"\n      >\n        <button class=\"big-text lpad-10\" id=\"try-again\">Try Again?</button>\n      </div>\n  ");
+    var tryAgainButton = document.getElementById("try-again");
+    tryAgainButton.addEventListener("click", function () {
+        resetGame();
+        elem.innerHTML = ""; // Clear the container
+        elem.innerHTML = gameBoard;
+    });
+}
 function randomNumber(n) {
     var result = Math.floor(Math.random() * n);
     return result;
@@ -40,15 +89,6 @@ function getComputerChoice() {
     var choice = Choice[choices[index]];
     return choice;
 }
-// function getPlayerChoice(): Choice {
-//   const playerChoice = prompt("Choose: Rock, Paper, or Scissors");
-//   if (playerChoice === null) {
-//     throw new Error("Must choose Rock, Paper, or Scissors");
-//   } else {
-//     let choice = Choice[playerChoice];
-//     return choice;
-//   }
-// }
 function playRound(playerSelection, computerSelection) {
     switch (playerSelection) {
         case Choice.Rock:
@@ -78,7 +118,7 @@ function playRound(playerSelection, computerSelection) {
         case Choice.Scissors:
             switch (computerSelection) {
                 case Choice.Scissors:
-                    console.log("It's a tie!");
+                    console.log("It's a ie!");
                     return 0;
                 case Choice.Rock:
                     console.log("You lose! Rock beats Scissors");
@@ -89,19 +129,3 @@ function playRound(playerSelection, computerSelection) {
             }
     }
 }
-// function game(numGames: number): undefined {
-//   let score = 0;
-//   for (let i = 0; i < numGames; i++) {
-//     let playerChoice = getPlayerChoice();
-//     let computerChoice = getComputerChoice();
-//     score += playRound(playerChoice, computerChoice);
-//   }
-//   if (score === 0) {
-//     console.log("Game over: it's a tie!");
-//   } else if (score > 0) {
-//     console.log("Game over: you won!");
-//   } else {
-//     console.log("Game over: you lost :(");
-//   }
-// }
-// game(1)
--- rock-paper-scissors.ts
+++ rock-paper-scissors.ts
@@ -4,30 +4,115 @@ enum Choice {
   Scissors = "Scissors",
 }
 
+const startButton = document.getElementById("start-game")!;
+const tryAgain = document.getElementById("try-again")!;
+let gameBoard = `<div id="game-container">
+      <div
+        class="flex flex-wrap gap50 justify-center bottom-margin-20 top-margin-50"
+      >
+        <div>
+          <h2>Human</h2>
+          <div class="flex justify-center gap5 border-solid padding-100">
+            <button id="Rock">Rock</button>
+            <button id="Paper">Paper</button>
+            <button id="Scissors">Scissors</button>
+          </div>
+          <h3 id="human-score">Score:</h3>
+        </div>
+        <div>
+          <h2>Computer</h2>
+          <div class="flex justify-center gap5 border-solid padding-100">
+            <button id="cRock">Rock</button>
+            <button id="cPaper">Paper</button>
+            <button id="cScissors">Scissors</button>
+          </div>
+          <h3 id="computer-score">Score:</h3>
+        </div>
+      </div>
+      <h3 id="roundAnnouncement"></h3>
+    </div>`;
+
+startButton.addEventListener("click", () => {
+  let elem = document.getElementById("start-game-div")!;
+  console.log(elem);
+  elem.parentNode?.removeChild(elem);
+  elem = document.getElementById("container")!;
+  elem.innerHTML = gameBoard;
+});
+
 let playerScore = 0;
 let computerScore = 0;
+let announceText: string;
 let nTies = 0;
-const buttons = document.querySelectorAll("button");
-buttons.forEach((button) => {
-  button.addEventListener("click", () => {
-    let playerChoice = Choice[button.id];
-    let computerChoice = getComputerChoice();
-    let score = playRound(playerChoice, computerChoice);
-    if (score === 1) {
-      playerScore +=1
-    } else if (score === -1) {
-      computerScore +=1
-    } else {
-      nTies += 1
+
+document
+  .getElementById("container")!
+  .addEventListener("click", function (event: MouseEvent) {
+    const button = event.target as HTMLButtonElement;
+    console.log(button);
+    if (
+      button &&
+      button instanceof HTMLButtonElement &&
+      button.id != "start-game" &&
+      button.id != "try-again"
+    ) {
+      let playerChoice = Choice[button.id];
+      let computerChoice = getComputerChoice();
+      let score = playRound(playerChoice, computerChoice);
+      if (score === 1) {
+        playerScore += 1;
+        announceText = `You win! ${playerChoice} beats ${computerChoice}`;
+      } else if (score === -1) {
+        computerScore += 1;
+        announceText = `You lose :( ${computerChoice} beats ${playerChoice}`;
+      } else {
+        nTies += 1;
+        announceText = `It's a tie: You both chose ${computerChoice}`;
+      }
+      let humanResult = `Score: ${playerScore}`;
+      let computerResult = `Score: ${computerScore}`;
+      let humanScoreDisplay = document.getElementById("human-score")!;
+      let computerScoreDisplay = document.getElementById("computer-score")!;
+      let roundAnnouncement = document.getElementById("roundAnnouncement")!;
+      if (playerScore === 5) {
+        endGame("Humans Win!");
+      } else if (computerScore === 5) {
+        endGame("Machines Win :(");
+      } else {
+        roundAnnouncement.textContent = announceText;
+      }
+      humanScoreDisplay.textContent = humanResult;
+      computerScoreDisplay.textContent = computerResult;
     }
-    let humanResult = `Human: ${playerScore}`
-    let computerResult = `Computer : ${computerScore}`
-    let humanScoreDisplay = document.getElementById("human-score")!;
-    let computerScoreDisplay = document.getElementById("computer-score")!;
-    humanScoreDisplay.textContent = humanResult
-    computerScoreDisplay.textContent = computerResult
   });
-});
+
+function resetGame() {
+  playerScore = 0;
+  computerScore = 0;
+  nTies = 0;
+}
+
+function endGame(text: string): undefined {
+  let elem = document.getElementById("game-container")!;
+  elem.parentNode?.removeChild(elem);
+  elem = document.getElementById("container")!;
+  elem.innerHTML = `<h2>GAME OVER</h2>
+  <h3>${text}</h3>
+  <div
+        id="start-game-div"
+        class="flex justify-center align-center full-height"
+      >
+        <button class="big-text lpad-10" id="try-again">Try Again?</button>
+      </div>
+  `;
+
+  const tryAgainButton = document.getElementById("try-again")!;
+  tryAgainButton.addEventListener("click", () => {
+    resetGame();
+    elem.innerHTML = ""; // Clear the container
+    elem.innerHTML = gameBoard 
+  });
+}
 
 
 function randomNumber(n: number): number {
@@ -41,16 +126,6 @@ function getComputerChoice(): Choice {
   let choice = Choice[choices[index]];
   return choice;
 }
-
-// function getPlayerChoice(): Choice {
-//   const playerChoice = prompt("Choose: Rock, Paper, or Scissors");
-//   if (playerChoice === null) {
-//     throw new Error("Must choose Rock, Paper, or Scissors");
-//   } else {
-//     let choice = Choice[playerChoice];
-//     return choice;
-//   }
-// }
 
 function playRound(playerSelection: Choice, computerSelection: Choice): number {
   switch (playerSelection) {
@@ -81,7 +156,7 @@ function playRound(playerSelection: Choice, computerSelection: Choice): number {
     case Choice.Scissors:
       switch (computerSelection) {
         case Choice.Scissors:
-          console.log("It's a tie!");
+          console.log("It's a ie!");
           return 0;
         case Choice.Rock:
           console.log("You lose! Rock beats Scissors");
@@ -92,21 +167,3 @@ function playRound(playerSelection: Choice, computerSelection: Choice): number {
       }
   }
 }
-
-// function game(numGames: number): undefined {
-//   let score = 0;
-//   for (let i = 0; i < numGames; i++) {
-//     let playerChoice = getPlayerChoice();
-//     let computerChoice = getComputerChoice();
-//     score += playRound(playerChoice, computerChoice);
-//   }
-//   if (score === 0) {
-//     console.log("Game over: it's a tie!");
-//   } else if (score > 0) {
-//     console.log("Game over: you won!");
-//   } else {
-//     console.log("Game over: you lost :(");
-//   }
-// }
-
-// game(1)
